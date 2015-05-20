f285985c61ff7eca68101eec0402d2bcd50a1dd1
Chris Scott <christocracy@gmail.com>
2015-05-20 11:45:37 -0400

    change to ssh url

--- package.js
+++ package.js
@@ -2,15 +2,17 @@ Package.describe({
   summary: "Cordova enabled background geolocation, even when closed/suspended",
   version: "0.0.7",
   name: "christocracy:cordova-background-geolocation",
-  git: "https://github.com/christocacy/meteor-cordova-geolocation-background"
+  git: "git@github.com:christocracy/cordova-background-geolocation.git"
 });
 
 Cordova.depends({
   // http://plugins.cordova.io/#/package/com.romainstrock.cordova.background-geolocation
-  'https://github.com/christocracy/cordova-background-geolocation.git': '0.4.0',
+  'git@github.com:christocracy/cordova-background-geolocation.git': '0.4.0',
   'org.apache.cordova.geolocation': '0.3.12',
   'org.apache.cordova.device': '0.2.11'
 });
+
+
 
 
 Package.onUse(function(api) {
@@ -23,6 +25,6 @@ Package.onUse(function(api) {
 
 Package.onTest(function(api) {
   api.use('tinytest');
-  api.use('christocracy:cordova-geolocation-background');
+  api.use('christocracy:cordova-background-geolocation');
   api.addFiles('cordova-geolocation-background-tests.js', 'client');
 });
